7635e1eeff95a720f85c49baa19d25e59def310f
Victor <victor.travassos@me.com>
2024-10-27 23:15:06 -0300

    url config, map / to home_page view

--- functional_tests.py
+++ functional_tests.py
@@ -10,10 +10,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_todo_list(self):
         self.browser.get("http://localhost:8000")
-
         self.assertIn("To-Do", self.browser.title)
-
-        self.fail("Finish the test!")
 
 if __name__ == "__main__":
     unittest.main()
--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,11 @@
+from django.http import HttpRequest
 from django.test import TestCase
+from lists.views import home_page
 
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+    def test_home_page_returns_correct_html(self):
+        response = self.client.get("/")
+        self.assertContains(response, "<title>To-Do lists</title>")
+        self.assertContains(response, "<html>")
+        self.assertContains(response, "</html>")
+
