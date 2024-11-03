2db831ae661a585f6562d84dcf3662f5330962a1
Victor <victor.travassos@me.com>
2024-11-03 00:57:14 -0300

    Model for the list Items

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.http import HttpRequest
 from django.test import TestCase
 from lists.views import home_page
+from lists.models import Item
 
 class HomePageTest(TestCase):
     def test_uses_home_template(self):
@@ -11,3 +12,21 @@ class HomePageTest(TestCase):
         response = self.client.post("/", data={"item_text":"A new list item"})
         self.assertContains(response, "A new list item")
         self.assertTemplateUsed(response, "home.html")
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = "The first (ever) list item"
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "Item the second"
+        second_item.save()
+
+        saved_itens = Item.objects.all()
+        self.assertEqual(saved_itens.count(), 2)
+
+        first_saved_item = saved_itens[0]
+        second_saved_item = saved_itens[1]
+        self.assertEqual(first_saved_item.text, "The first (ever) list item")
+        self.assertEqual(second_saved_item.text, "Item the second")
