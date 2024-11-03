1fe9f2e51ff49be8d3b3e2bb3fff9e73883a812a
Victor <victor.travassos@me.com>
2024-11-03 00:23:36 -0300

    Refactor to reuse assert for rows

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_todo_list(self):
         self.browser.get("http://localhost:8000")
         self.assertIn("To-Do", self.browser.title)
@@ -23,18 +28,10 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(10)
 
-        # The page updates again, and now shows both items on her list
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertIn(
-            "1: Buy peacock feathers",
-            [row.text for row in rows],
-        )
-        self.assertIn(
-            "2: Use peacock feathers to make a fly",
-            [row.text for row in rows],
-        )
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
         self.fail("Finish")
+
 if __name__ == "__main__":
     unittest.main()
